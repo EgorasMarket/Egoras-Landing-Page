c733af940a5c06c0ec80661ab1ad81b712179cbf
Samuel Ifeanyi <samuelify225@gmail.com>
2021-12-17 12:54:35 +0100

    updated the ui

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -484,8 +484,37 @@ const Landing = () => {
       </section>
       <a href="https://earn.egoras.com" className="earn_link" target="_blank">
         <button className="earn_btn">
-          {" "}
-          <p className="refff">Referral program</p>
+          <div class="about">
+            <a
+              class="bg_links social portfolio"
+              href="https://www.rafaelalucas.com"
+              target="_blank"
+            >
+              <span class="icon"></span>
+            </a>
+            <a
+              class="bg_links social dribbble"
+              href="https://dribbble.com/rafaelalucas"
+              target="_blank"
+            >
+              <span class="icon"></span>
+            </a>
+            <a
+              class="bg_links social linkedin"
+              href="https://www.linkedin.com/in/rafaelalucas/"
+              target="_blank"
+            >
+              <span class="icon"></span>
+            </a>
+            <a class="bg_links logo"></a>
+          </div>
+          {/* <!-- end about --> */}
+
+          <div class="button">
+            <a class="link">Referral program</a>
+          </div>
+
+          {/* <p className="refff">Referral program</p> */}
         </button>
       </a>
     </div>
